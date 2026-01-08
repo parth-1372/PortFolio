4c956677ff523930d1bfa9f16e21965011f8b372
Parth <parthmungra1372@gmail.com>
2026-01-08 14:30:00 +0530

    style: Add smooth animations and blob effects

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,8 +36,18 @@ export default {
           '0%': { opacity: '0', transform: 'translateY(20px)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
         },
-      }
+        blob: {
+          "0%": { transform: "translate(0px, 0px) scale(1)" },
+          "33%": { transform: "translate(30px, -50px) scale(1.1)" },
+          "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
+          "100%": { transform: "translate(0px, 0px) scale(1)" }
+        }
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-out forwards',
+        'slide-up': 'slideUp 0.5s ease-out forwards',
+        'blob': "blob 7s infinite",
+      },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+  }
